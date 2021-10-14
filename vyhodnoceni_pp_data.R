69b25142da789ed081c6028f949db7551a030da9
Beitlerová Hana <beitlerova.hana@vumop.local>
2021-10-14 18:40:30 +0200

    grafy průtoků

--- vyhodnoceni_pp_data.R
+++ vyhodnoceni_pp_data.R
@@ -55,7 +55,7 @@ N50_venp <- N50_venp %>% ungroup() %>% group_by(Time, ID) %>% count(Sedbudget=su
 ### nazvy variant - MENI UZIVATEL
 
 varianty <- list(N10, N10_venp, N10_udol, N10_svod, N50, N50_venp, N50_udol, N50_svod )
-names(varianty) <- c("N10", "N10_udol", "N10_svod", "N10_venp", "N50", "N50_udol", "N50_svod", "N50_venp")
+names(varianty) <- c("N10", "N10_venp", "N10_udol", "N10_svod", "N50", "N50_venp", "N50_udol", "N50_svod")
  
 ### udaje o simulaci - MENI UZIVATEL
 
@@ -99,31 +99,29 @@ write.csv(dfSumQ,paste0("u:/Vyzkum/17318_NAZV_E3D/3_zpracovani/METODIKA/LUBY_pri
 write.csv(dfMaxQ,paste0("u:/Vyzkum/17318_NAZV_E3D/3_zpracovani/METODIKA/LUBY_priradovka/E3D/Vystupy/results/", "vysledky_pp_maxQ3.csv"))
 write.csv(dfSumS,paste0("u:/Vyzkum/17318_NAZV_E3D/3_zpracovani/METODIKA/LUBY_priradovka/E3D/Vystupy/results/", "vysledky_pp_sumSed3.csv"))
 
-### vytvoří graf průtoků
+### vytvoří grafy průtoků
+#v jednom grafu je 1 varianta a všechny UP
 
 for (i in 1:length(varianty)) {
-  maxQ <- varianty[[i]] %>% group_by(ID) %>% mutate(Q = lead(Runoff,1) - Runoff) %>% count(max(Q,na.rm=TRUE))
-  maxQ <- maxQ$`max(Q, na.rm = TRUE)`
-  maxQ <- maxQ * rozliseni / 5 / 60 #prepocet ma m3/s
-  dfMaxQ[1:length(maxQ),i] <- maxQ
-    }
-
-N10_sum <- N10 %>% ungroup() %>% group_by(Time, ID) %>% count(Sedbudget=sum(Sedbudget),Runoff=sum(Runoff),Sedvol=sum(Sedvol),Sedconc=sum(Sedconc),
-                                                        Clay=sum(Clay),Silt=sum(Silt),Totero=sum(Totero),Totdep=sum(Totdep),Netero=sum(Netero),
-                                                        ChRunoff=sum(ChRunoff),ChSedvol=sum(ChSedvol),ChNetEro=sum(ChNetEro),
-                                                        ChClay=sum(ChClay),ChSilt=sum(ChSilt),Q_Cell=sum(Q_Cell)) #sečte pro každý krok hodnotu jednoho ID (je-li UP definován více pixely jednotného ID)
-
-
-Q <- N10_sum %>% group_by(ID) %>% mutate(Q = lag(lead(Runoff,1) - Runoff), Q_ms = Q * rozliseni / 5 / 60) #výpočet Runoff pro každý krok ne kumulativně
-
-Qpp <- Q[Q$ID==1,]
-plot(Qpp$Time, Qpp$Q_ms,col="white")
-
-for (i in 1:pp) {
-  Qpp <- Q[Q$ID==i,]
-  lines(Qpp$Time, Qpp$Q_ms, col=i)
+  jpeg(file=paste0("graf_prutoku",i,".jpeg"))
+  #generovani prutoku - rada prutoku v m3*s-1 za kazdy krok (nekumulativne)
+  Prutoky <- varianty[[i]] %>% 
+    group_by(ID) %>% mutate(Q = lag(lead(Runoff,1) - Runoff), Q_ms = Q * rozliseni / 5 / 60) %>% 
+    select(Time, ID, Q_ms)
+  
+  #definice grafu
+  Prutokova_rada <- Prutoky[Prutoky$ID==1,]
+  plot(Prutokova_rada$Time, Prutokova_rada$Q_ms,col="white", xlab="čas", ylab='průtok (m3*s-1)')
+  
+  #generovani lajn pro jednotliva ID
+  for (j in 1:pp) {
+    Prutokova_rada <- Prutoky[Prutoky$ID==j,] #rada prutoku jen pro jedno ID
+    lines(Prutokova_rada$Time, Prutokova_rada$Q_ms, col=j) #pridani rady do grafu
+  }
+  legend("topright",legend=c("UP1 - kukuřice/tráva", "UP2 - tráva/intravilán", "UP3 - průleh"), col = c("black","red","green"), lty=c(1,1,1),)
+  #ulozeni grafu
+  dev.off()
 }
-
 
 
 
